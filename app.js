08b613f8c2cdba5bb81e75d081dd58234176a939
Stefan Koev <st.koev@outlook.com>
2022-09-28 15:56:00 +0300

    implement javascritp, final commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('dark mode');
+const toggleBtn = document.querySelector(".btn");
+const articlesContainer = document.querySelector(".articles");
+
+toggleBtn.addEventListener("click", () => {
+  document.documentElement.classList.toggle("dark-theme");
+});
+
+const articlesData = articles
+  .map(({ title, date, length, snippet }) => {
+    const formatDate = moment(date).format("MMMM Do YYYY");
+    return `
+    <article class="post">
+        <h2>${title}</h2>
+        <div class="post-info">
+        <span>${formatDate}</span>
+        <span>${length} minutes read</span>
+        </div>
+        <p>
+        ${snippet}
+        </p>
+    </article>
+    `;
+  })
+  .join("");
+
+articlesContainer.innerHTML = articlesData;
+console.log(moment());
